eb36120d53fc510ba1b7bf801140f9a0bfb69a07
andy mars <andy9887776666@gmail.com>
2016-11-20 15:07:39 +0800

    Create maximumDepth.js

---
+++ src/maximumDepth.js
@@ -0,0 +1,20 @@
+/**
+ * 125ms
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (root === null) return 0;
+    
+    var leftDepth = maxDepth(root.left);
+    var rightDepth = maxDepth(root.right);
+    
+    return leftDepth > rightDepth ? (leftDepth + 1) : (rightDepth + 1);
+};
