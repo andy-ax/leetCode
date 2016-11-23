a74ca11147e30546363d3b5dc8dce7504ee2856d
andy mars <andy9887776666@gmail.com>
2016-11-23 20:06:46 +0800

    Create invertBinaryTree.js

---
+++ src/invertBinaryTree.js
@@ -0,0 +1,21 @@
+/**
+ * 100ms
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) return root;
+    var left = root.left;
+    root.left = root.right;
+    root.right = left;
+	invertTree(root.left);
+	invertTree(root.right);
+	return root;
+};
