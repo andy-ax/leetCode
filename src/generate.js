9ba6490cc23f0f972b1de086e0c13c9ba1e9a848
andy mars <andy9887776666@gmail.com>
2016-12-26 22:05:05 +0800

    Create generate.js

---
+++ src/generate.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    var init = function () {
+        var arr = [];
+        for (var i = 0; i < numRows; i++) {
+            if (i === 0) {
+                arr.push([1]);
+            } else {
+                arr.push(generateOne(arr[arr.length - 1]));
+            }
+        }
+        return arr;
+    };
+    var generateOne = function (arr) {
+        var newArr = [];
+        superposition(arr,0,function(x,y){
+            newArr.push(x+y);
+        });
+        newArr.push(1);
+        return newArr;
+    }
+    var superposition = function (arr, arrLeft, cb) {
+        var value = arrLeft;
+        arr.forEach(function (v, i) {
+            cb(value, v);
+            value = v;
+        });
+    }
+    return init();
+}
